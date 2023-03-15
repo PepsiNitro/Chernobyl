8584078651f2fb32a52cb4f534b5ab66fef7a208
Pepsi_Man <doomguyrisenow@gmail.com>
2023-03-15 17:19:22 +0000

    ws3ed

--- p/sw.js
+++ p/sw.js
@@ -1,12 +1,13 @@
-var CACHE_NAME = 'version6'; // bump this version when you make changes.
+var CACHE_NAME = 'version7'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     'index.html',
-    '../audio.html',
-    '../discretion.html',
-    '../js/jquery-3.3.1.min.js',
-    '../js/type.js',
-    '../js/visualizer.js',
+    'audio.html',
+    'discretion.html',
+
+    'js/jquery-3.3.1.min.js',
+    'js/type.js',
+    'js/visualizer.js',
     'css/style.css',
     'assets/logo-192.png'
 ];
