e9342801bb327db2b6ac9f7506741e4d1e7d96aa
Pepsi_Man <doomguyrisenow@gmail.com>
2023-03-17 18:05:46 +0000

    what now

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'version4'; // bump this version when you make changes.
+var CACHE_NAME = 'version5'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     'audio.html',
@@ -15,9 +15,6 @@ var urlsToCache = [
     'js/visualizer.js',
 
     'css/style.css',
-
-    'assets/logo-192.png',
-    'assets/logo-512.png',
 
     'assets/audios/200.mp3',
     'assets/audios/AudioLogBegin.mp3',
@@ -61,6 +58,9 @@ var urlsToCache = [
     'assets/videos/TerminalClassified.mp4',
     'assets/videos/TerminalRadiationLevelsBlue.mp4',
     'assets/videos/TerminalRadiationLevelsRed.mp4',
+
+    'assets/logo-192.png',
+    'assets/logo-512.png',
 
 ];
 
