f2ff5a55f854fab2d8da1c7615f3eb1b8c5fa08c
Pepsi_Man <doomguyrisenow@gmail.com>
2023-03-24 16:47:41 +0000

    please

--- sw.js
+++ sw.js
@@ -1,8 +1,9 @@
-var CACHE_NAME = 'version4'; // bump this version when you make changes.
+var CACHE_NAME = 'version5'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
     'audio.html',
     'discretion.html',
+    'index.html',
 
     'linked/200.html',
     'linked/fate.html',
