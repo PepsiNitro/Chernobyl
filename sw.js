48186fc9c70cc06e0c04a37f9a7f9b6b5e311774
Pepsi_Man <doomguyrisenow@gmail.com>
2023-03-24 13:02:42 +0000

    bruh

--- sw.js
+++ sw.js
@@ -1,67 +1,8 @@
-var CACHE_NAME = 'version1'; // bump this version when you make changes.
+var CACHE_NAME = 'version0'; // bump this version when you make changes.
 // Put all your urls that you want to cache in this array
 var urlsToCache = [
-    'audio.html',
-    'discretion.html',
     'index.html',
-
-    'linked/200.html',
-    'linked/fate.html',
-    'linked/links.html',
-    'linked/video.html',
-
-    'js/jquery-3.3.1.min.js',
-    'js/type.js',
-    'js/visualizer.js',
-
-    'css/style.css',
-
-    'assets/audios/200.mp3',
-    'assets/audios/AudioLogBegin.mp3',
-    'assets/audios/TheFateOfLiquidators.mp3',
-    'assets/audios/Warning.mp3',
-
-    'assets/images/3828.jpg',
-    'assets/images/ControlCenter.jpg',
-    'assets/images/DeconstructionOfVillages.jpg',
-    'assets/images/FalloutRadius.jpg',
-    'assets/images/FlightOverChernobly.jpg',
-    'assets/images/HelicopterCrash.png',
-    'assets/images/InsideChernobly.jpg',
-    'assets/images/Liquidators.jpg',
-    'assets/images/LiquidatorsOfChernobyl.jpg',
-    'assets/images/MixedMedia.jpg',
-    'assets/images/NeverBefore.jpg',
-    'assets/images/PCPEvacuation.jpg',
-    'assets/images/RoofCleaning.png',
-    'assets/images/RoomCleanUp.jpg',
-    'assets/images/TerminalCall.png',
-    'assets/images/TerminalClassified.jpg',
-    'assets/images/TerminalRadiationLevelsBlue.jpg',
-    'assets/images/TerminalRadiationLevelsRed.jpg',
-
-    'assets/videos/3828.mp4',
-    'assets/videos/ControlCenter.mp4',
-    'assets/videos/DeconstructionOfVillages.mp4',
-    'assets/videos/FalloutRadius.mp4',
-    'assets/videos/FlightOverChernobly.mp4',
-    'assets/videos/HelicopterCrash.mp4',
-    'assets/videos/InsideChernobly.mp4',
-    'assets/videos/Liquidators.mp4',
-    'assets/videos/LiquidatorsOfChernobyl.mp4',
-    'assets/videos/MixedMedia.mp4',
-    'assets/videos/NeverBefore.mp4',
-    'assets/videos/PCPEvacuation.mp4',
-    'assets/videos/RoofCleaning.mp4',
-    'assets/videos/RoomCleanUp.mp4',
-    'assets/videos/TerminalCall.mp4',
-    'assets/videos/TerminalClassified.mp4',
-    'assets/videos/TerminalRadiationLevelsBlue.mp4',
-    'assets/videos/TerminalRadiationLevelsRed.mp4',
-
-    'assets/logo-192.png',
-    'assets/logo-512.png',
-
+    'assets/logo-192.png'
 ];
 
 // Install the service worker and open the cache and add files mentioned in array to cache
